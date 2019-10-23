9521e86a33a6eda14bdc4e2d6b235b93c308681a
Destructive code <rulitosxd1@gmail.com>
2019-10-23 18:34:00 -0500

    jquery class

---
+++ JQuery/DOM_script_c.js
@@ -0,0 +1,46 @@
+/*$(document).ready(()=>
+    {
+        $('.parrafo1').hide();
+    }
+); //Carga cuando el documento esté listo
+*/
+
+// Para traer una clase con $ es con $('.clase')
+
+//$('p').addClass('info'); //Añade una nueva clase a los parrafos llamada info
+
+//$('p').addClass('info').css('background', 'red');  
+//.css('primero','segundo') primero= propiedad, segundo= valor
+// con .css('propiedad') es un get de la propiedad
+
+//$('#parrafo2') // Para traer un id con $ es con $('#clase')
+
+//$('#parrafo2').prev().css('color', 'red')
+//$('#parrafo2').next().css('color', 'red')// modifica los siguientes o anteriores dependiendo de la sintaxys
+
+//$('#parrafo2').siblings().css('color', 'blue') //modifica todos los que están al mismo nivel del objeto sin incluirlo a él
+
+//$('#parrafo2').siblings().addBack().css('color', 'blue')//modifica todos los que están al mismo nivel del objeto incluyendolo
+
+
+
+// Crear variables
+
+
+//var $p = $('p'); // variable JQuery al incluir $ antes del nombre
+
+//var $div = $('div');
+
+//$div.children() // Todos los hijos del tag div
+
+//$div.children('ol') // Todos los hijos del tag div dentro de ol
+// ol = lista ordenada HTML
+
+
+//$div.atrr("class", "info") // Adiciona una clase info
+
+//$('elemento').on("evento", handler) // on = listener   
+//evento = event, click, mouseevent... 
+//handler = function
+
+
